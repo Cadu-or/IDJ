f7e43f7fbbd41fb84ee4728153ae04e20077bdea
Carlos Eduardo <52487636+Cadu-or@users.noreply.github.com>
2022-07-25 16:18:05 -0300

    Update main.cpp

--- src/components/main.cpp
+++ src/components/main.cpp
@@ -1,8 +1,8 @@
 #include "../include/Game.h"
 
 int main(int argc, char** argv){
-    Game *game = Game::GetInstance();
-    game->Run();
+    Game game = Game::GetInstance();
+    game.Run();
 
     return 0;
 }
